237388e189b9c7a4e5253cad3b98b4e0b4c35616
ahmed tarek <ahmedtrk97@gmail.com>
2020-06-22 10:38:35 +0200

    connecting note list activity with note activity

---
+++ app/src/main/java/com/example/notekeeper/NoteListActivity.java
@@ -0,0 +1,61 @@
+package com.example.notekeeper;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class NoteListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        intializeDisplayContent();
+    }
+
+    private void intializeDisplayContent() {
+        ListView listNotes = findViewById(R.id.list_notes);
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        ArrayAdapter<NoteInfo> adapterNotes = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,notes);
+        listNotes.setAdapter(adapterNotes);
+        //AdapterView.OnItemClickListener is called an anonymous class
+        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            //the parameter position is the position of where the user clicked
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                //to launch other activity we need an intent
+                // we call this code from the NodeList activity  and Activity can be based as context
+                // this  should refer to the NoteListActivity class but it actually refers to the anonymous class so we should not use it
+                // we instead use NoteListActivity.this which refers to NoteListActivity
+                // second parameter we want a class information about the activity we will launch
+                Intent intent = new Intent(NoteListActivity.this,NoteActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
